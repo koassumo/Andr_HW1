e393d7c3f885425e50f0431891d2a1d20986c368
koassumo <0770407@gmail.com>
2020-03-19 00:53:28 +0300

    урок 3. Сохранение данных при повороте с использованием onStateInstanceState (с урока, для ДЗ не нужно)

--- app/src/main/java/ru/geekbrains/android1/hw1/SitySelect.java
+++ app/src/main/java/ru/geekbrains/android1/hw1/SitySelect.java
@@ -1,19 +1,29 @@
 package ru.geekbrains.android1.hw1;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SitySelect extends AppCompatActivity {
 
+    private static final String TAG = "SitySelect";
     private CheckBox checkBoxAtmoPressure;
     private TextView textViewAtmoInfo;
 
     private CheckBox checkBoxWind;
     private TextView textViewWindInfo;
+
+    private Button buttonTTTT;
+    private TextView textViewTTTT;
+    private final String counterDataKey = "counterDataKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,17 @@ public class SitySelect extends AppCompatActivity {
         initViews();
         setOnСheckBoxAtmoPressure();
         setOnСheckBoxWind();
+
+        initIncreaseBtnBehavior();
+
+        String instanceState;
+        if (savedInstanceState == null) {
+            instanceState = "Первый запуск!";
+        } else {
+            instanceState = "Повторный запуск!";
+        }
+        Toast.makeText(getApplicationContext(), instanceState + " - onCreate()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, instanceState + " - onCreate()");
     }
 
     private void initViews() {
@@ -30,6 +51,20 @@ public class SitySelect extends AppCompatActivity {
 
         checkBoxWind = findViewById(R.id.checkBoxWind);
         textViewWindInfo = findViewById(R.id.textViewWindInfo);
+
+        buttonTTTT = findViewById(R.id.button_tt);
+        textViewTTTT = findViewById(R.id.textView_TT);
+    }
+
+    private void initIncreaseBtnBehavior() {
+        buttonTTTT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int counterValue = Integer.parseInt(textViewTTTT.getText().toString());
+                String newTextValue = String.valueOf(++counterValue);
+                textViewTTTT.setText(newTextValue);
+            }
+        });
     }
 
     private void setOnСheckBoxAtmoPressure() {
@@ -62,4 +97,47 @@ public class SitySelect extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle saveInstanceState){
+        super.onRestoreInstanceState(saveInstanceState);
+        //DataContainer container = (DataContainer)saveInstanceState.getSerializable(counterDataKey);
+
+        String text = saveInstanceState.getString(counterDataKey, "0");
+        textViewTTTT.setText(text);
+
+        Toast.makeText(getApplicationContext(), "Повторный запуск!! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle saveInstanceState){
+        Toast.makeText(getApplicationContext(), "заебись - onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+
+        String text = textViewTTTT.getText().toString();
+        saveInstanceState.putString(counterDataKey, text);
+
+        super.onSaveInstanceState(saveInstanceState);
+
+//        saveInstanceState.putSerializable(counterDataKey, DataContainer.getInstance());
+    }
 }
