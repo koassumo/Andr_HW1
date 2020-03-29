cc8480b8f1fb3f8d97625a9465609f7d1589b027
koassumo <0770407@gmail.com>
2020-03-30 01:38:01 +0300

    constraintLayout не хочет во фрагмент

---
+++ app/src/main/java/ru/geekbrains/android1/hw1/InfoDeskFragment.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.android1.hw1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class InfoDeskFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info_desk, container, false);
+    }
+}
