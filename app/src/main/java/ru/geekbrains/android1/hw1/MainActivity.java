b2a98e9b532e0295f2160f602b5c9440d7364c7e
koassumo <0770407@gmail.com>
2020-04-05 19:50:39 +0300

    ДЗ 7 и 8.

--- app/src/main/java/ru/geekbrains/android1/hw1/MainActivity.java
+++ app/src/main/java/ru/geekbrains/android1/hw1/MainActivity.java
@@ -4,14 +4,23 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,12 +28,18 @@ public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_CODE = 1;
 
     private TextView townTextView;
-    private TextView degreesTextView;
+    private TextView temperatureTextView;
     private TextView pressureTextView;
     private TextView windTextView;
+    private TextView skyTexView;
+    private ImageView skyImageView;
     private boolean isPressureShow = true;
     private boolean isWindShow = true;
     private MaterialButton goOptionsSelectActivityBtn;
+
+    private final Handler handler = new Handler();
+    private final static String LOG_TAG = MainActivity.class.getSimpleName();
+    Typeface weatherFont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +52,124 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         townTextView = findViewById(R.id.townTextView);
-        degreesTextView = findViewById(R.id.degreesTextView);
+        temperatureTextView = findViewById(R.id.temperatureTextView);
         pressureTextView = findViewById(R.id.pressureTextView);
         windTextView = findViewById(R.id.windTextView);
+        skyTexView = findViewById(R.id.skyTextView);
+        skyImageView = (ImageView) findViewById(R.id.skyImageView);
         goOptionsSelectActivityBtn = findViewById(R.id.goOptionsSelectActivityBtn);
+        updateWeatherData(townTextView.getText().toString());
+    }
+
+    private void updateWeatherData(final String city) {
+        new Thread() {
+            @Override
+            public void run() {
+                final JSONObject jsonObject = WeatherDataLoader.getJSONData(city);
+                if(jsonObject == null) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), R.string.place_not_found,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            renderWeather(jsonObject);
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
+
+    private void renderWeather(JSONObject jsonObject) {
+        Log.d(LOG_TAG, "json: " + jsonObject.toString());
+        try {
+            JSONObject details = jsonObject.getJSONArray("weather").getJSONObject(0);
+            JSONObject main = jsonObject.getJSONObject("main");
+
+            setPlaceName(jsonObject);
+            setDetails(details, main);
+            setCurrentTemp(main);
+            setUpdatedText(jsonObject);
+            setWeatherIcon(details.getInt("id"),
+                    jsonObject.getJSONObject("sys").getLong("sunrise") * 1000,
+                    jsonObject.getJSONObject("sys").getLong("sunset") * 1000);
+        } catch (Exception exc) {
+            exc.printStackTrace();
+            Log.e(LOG_TAG, "One or more fields not found in the JSON data");
+        }
+    }
+
+    private void setPlaceName(JSONObject jsonObject) throws JSONException {
+        String cityText = jsonObject.getString("name").toUpperCase() + ", "
+                + jsonObject.getJSONObject("sys").getString("country");
+        townTextView.setText(cityText);
+    }
+
+    private void setDetails(JSONObject details, JSONObject main) throws JSONException {
+        String detailsText = details.getString("description").toUpperCase() + "\n" + main.getString("pressure") + "hPa";
+        pressureTextView.setText(detailsText);
+    }
+
+    private void setCurrentTemp(JSONObject main) throws JSONException {
+        String currentTextText = String.format(Locale.getDefault(), "%.2f", main.getDouble("temp"));
+        temperatureTextView.setText(currentTextText);
+    }
+
+    private void setUpdatedText(JSONObject jsonObject) throws JSONException {
+        DateFormat dateFormat = DateFormat.getDateTimeInstance();
+        String updateOn = dateFormat.format(new Date(jsonObject.getLong("dt") * 1000));
+        String updatedText = "Last update: " + updateOn;
+        windTextView.setText("100");
+    }
+
+    private void setWeatherIcon(int actualId, long sunrise, long sunset) {
+        int id = actualId / 100;
+        String icon = "";
+
+        if(actualId == 800) {
+            long currentTime = new Date().getTime();
+            if(currentTime >= sunrise && currentTime < sunset) {
+                icon = "\u2600";
+                //icon = getString(R.string.weather_sunny);
+            } else {
+                icon = getString(R.string.weather_clear_night);
+            }
+        } else {
+            switch (id) {
+                case 2: {
+                    icon = getString(R.string.weather_thunder);
+                    break;
+                }
+                case 3: {
+                    icon = getString(R.string.weather_drizzle);
+                    break;
+                }
+                case 5: {
+                    icon = getString(R.string.weather_rainy);
+                    break;
+                }
+                case 6: {
+                    icon = getString(R.string.weather_snowy);
+                    break;
+                }
+                case 7: {
+                    icon = getString(R.string.weather_foggy);
+                    break;
+                }
+                case 8: {
+                    icon = "\u2601";
+                    // icon = getString(R.string.weather_cloudy);
+                    break;
+                }
+            }
+        }
+        skyTexView.setText(icon);
     }
 
     private void setOnGoOptionsSelectBtnClick() {
@@ -68,11 +197,9 @@ public class MainActivity extends AppCompatActivity {
             isWindShow = data.getBooleanExtra(Constants.WIND_IS_CHECKED_KEY, true);
             if (isPressureShow) pressureTextView.setVisibility(View.VISIBLE); else pressureTextView.setVisibility(View.GONE);
             if (isWindShow) windTextView.setVisibility(View.VISIBLE); else windTextView.setVisibility(View.GONE);
-
         }
+        updateWeatherData(townTextView.getText().toString());
     }
-
-
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle saveInstanceState){
